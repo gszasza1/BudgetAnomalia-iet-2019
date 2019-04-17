cc9bc95b3f59b6ef7edb3221d8ce1c8a14cb555a
gszasza1 <gszasza1@gmail.com>
2019-04-17 23:03:49 +0200

    Initial test

--- src/com/complexible/common/csv/CSV2RDF.java
+++ src/com/complexible/common/csv/CSV2RDF.java
@@ -125,7 +125,7 @@ public class CSV2RDF implements Runnable {
 		System.out.printf("Converted %,d rows to %,d triples%n", inputRows, outputTriples);
 	}
 
-	private static char toChar(String value) {
+	public char toChar(String value) {
 		Preconditions.checkArgument(value.length() == 1, "Expecting a single character but got %s", value);
 		return value.charAt(0);
 	}
