64056058fe4d810ca754109818b72bf327c9ff8e
suledani <suledani97@gmail.com>
2019-04-30 17:09:07 +0200

    Fixed Codacy bugs in test.

--- src/test/TestMessage.java
+++ src/test/TestMessage.java
@@ -1,10 +1,12 @@
+package test;
+
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TestMessage {
 
     @Test
-    public void TestWelcomeMessage() {
+    public void testWelcomeMessage() {
 
         assertEquals("welcome", "welcome");
     }
