0612d0ec406a8034d3a428598f7d3c4a802f3b87
suledani <suledani97@gmail.com>
2019-05-18 13:36:20 +0200

    Created test for too many/missing arguments.

--- src/hu/bme/mit/budgetanomalia/test/TestExceptions.java
+++ src/hu/bme/mit/budgetanomalia/test/TestExceptions.java
@@ -4,8 +4,9 @@ import com.complexible.common.csv.CSV2RDF;
 import io.airlift.command.Cli;
 import io.airlift.command.Help;
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
-import java.io.File;
 import java.lang.IllegalArgumentException;
 
 public class TestExceptions {
@@ -13,18 +14,27 @@ public class TestExceptions {
     private String csvFile = "examples/cars/cars.csv";
     private String outputFile = "examples/cars/output.ttl";
 	private String argumentDummy = "examples/cars/cars.ttl";
+	private String fileDoesNotExist = "examples/cars/error.ttl";
+	private String separator = "dummy";
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testTooManyArguments(){
+    @Test
+    public void testTooManyArguments() throws IllegalArgumentException{
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Too many arguments");
         Cli.<Runnable> builder("csv2rdf").withDescription("Converts a CSV file to RDF based on a given template")
                 .withDefaultCommand(CSV2RDF.class).withCommand(CSV2RDF.class).withCommand(Help.class)
                 .build().parse(templateFile, csvFile, outputFile, argumentDummy).run();
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testNotEnoughArguments(){
+    @Test
+    public void testNotEnoughArguments() throws IllegalArgumentException{
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Missing arguments");
         Cli.<Runnable> builder("csv2rdf").withDescription("Converts a CSV file to RDF based on a given template")
                 .withDefaultCommand(CSV2RDF.class).withCommand(CSV2RDF.class).withCommand(Help.class)
                 .build().parse(templateFile, csvFile).run();
-    }
+    }		
 }
