ff5afe50ec9cf2f59431ec12bc01a75b3e81b95a
suledani <suledani97@gmail.com>
2019-05-18 14:38:29 +0200

    Created test for checking correct error handling

--- src/hu/bme/mit/budgetanomalia/test/TestExceptions.java
+++ src/hu/bme/mit/budgetanomalia/test/TestExceptions.java
@@ -3,9 +3,12 @@ package hu.bme.mit.budgetanomalia.test;
 import com.complexible.common.csv.CSV2RDF;
 import io.airlift.command.Cli;
 import io.airlift.command.Help;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.Test;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
+import static org.junit.Assert.assertEquals;
 
 public class TestExceptions {
     private String templateFile = "examples/cars/cars.ttl";
@@ -32,5 +35,17 @@ public class TestExceptions {
         Cli.<Runnable> builder("csv2rdf").withDescription("Converts a CSV file to RDF based on a given template")
                 .withDefaultCommand(CSV2RDF.class).withCommand(CSV2RDF.class).withCommand(Help.class)
                 .build().parse(templateFile, csvFile).run();
-    }		
+    }
+
+	@Test
+	public void testExceptionCatching(){
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setErr(new PrintStream(outContent));
+		Cli.<Runnable> builder("csv2rdf").withDescription("Converts a CSV file to RDF based on a given template")
+                .withDefaultCommand(CSV2RDF.class).withCommand(CSV2RDF.class).withCommand(Help.class)
+                .build().parse(templateFile, csvFile, outputFile, "--separator", "asd").run();
+		String str = outContent.toString();
+		String[] arrOfStr = str.split(":", 2);
+		assertEquals("ERROR", arrOfStr[0]);
+	}
 }
